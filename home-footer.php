53fe7c3493db006241916a69bbe54467ec4e29da
tmyie <hello@tomjhume.com>
2014-02-12 10:22:25 +0000

    Added home-footer

---
+++ home-footer.php
@@ -0,0 +1,40 @@
+	<div class="clear"></div>
+
+	<div class="footer">
+
+		<div class="col25">
+			<img src="images/how-logo-small.jpg" alt="" class="footer-logo">
+			<p>
+				Hour of Writes is a weekly peer 
+reviewed writing competition. Enter 
+now for a chance to be published!
+			</p>
+		</div>
+		<div class="col25">
+			<p>About us</p>
+			<p>Contact us</p>
+			<p>Terms and Conditions</p>
+		</div>
+		<div class="col25">
+				<p class="sm">
+					<img src="images/facebook.jpg" alt="">
+					<span>Like us on Facebook</span>
+				</p>
+				<p class="sm">
+					<img src="images/twitter.jpg" alt="">
+					Follow us on Twitter
+				</p>
+				<p class="sm">
+					<img src="images/instagram.jpg" alt="">
+					Follow us on Facebook
+				</p>
+		</div>
+		<div class="col25">
+			<p>How it works</p>
+			<p>Hour of Writes 2013 &copy;</p>
+
+		</div>
+	</div>
+
+</body>
+</html>
